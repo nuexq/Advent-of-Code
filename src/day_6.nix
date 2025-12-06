8caf5c853d7a611a42c7b468d6302397c7e547aa
nuexq <nuexqq@gmail.com>
2025-12-06 15:19:18 +0100

    feat: day 6 part 2 (worst code ever)

--- src/day_6.nix
+++ src/day_6.nix
@@ -1,7 +1,7 @@
 let
   lib = import <nixpkgs/lib>;
-  inherit (lib) strings foldl' init last range;
-  inherit (builtins) map filter length elemAt;
+  inherit (lib) strings foldl' init last range substring stringToCharacters;
+  inherit (builtins) map filter length elemAt stringLength head;
 
   input = ''
     123 328  51 64 
@@ -9,7 +9,6 @@ let
       6 98  215 314
     *   +   *   +  
   '';
-  # input = builtins.readFile ./input.txt;
 
   cleanedInput = filter (x: x != "") (strings.splitString "\n" input);
 
@@ -24,17 +23,44 @@ let
         map (j: (elemAt (elemAt numbers j) i)) (range 0 ((length numbers) - 1));
     in col) (range 0 ((length (elemAt numbers 0)) - 1));
 
-  part1 = foldl' (total: idx:
+  compute = problems:
+    foldl' (total: idx:
+      let
+        numbers = elemAt problems idx;
+        operation = elemAt operations idx;
+
+        ints = map strings.toInt numbers;
+
+        result = if operation == "+" then
+          foldl' (a: b: a + b) 0 ints
+        else
+          foldl' (a: b: a * b) 1 ints;
+      in total + result) 0 (range 0 (length problems - 1));
+
+  paddedProblems = foldl' (acc: problem:
     let
-      numbers = elemAt problems idx;
-      operation = elemAt operations idx;
-
-      ints = map strings.toInt numbers;
-
-      result = if operation == "+" then
-        builtins.foldl' (a: b: a + b) 0 ints
+      start = if acc == [ ] then
+        0
       else
-        builtins.foldl' (a: b: a * b) 1 ints;
-    in total + result) 0 (range 0 (length problems - 1));
+        (foldl' (sum: col: sum + stringLength (head col)) 0 acc) + (length acc);
 
-in { inherit part1; }
+      width =
+        foldl' (m: s: if stringLength s > m then stringLength s else m)
+        0 problem;
+
+      col = map (row: substring start width row) (init cleanedInput);
+    in acc ++ [ col ]) [ ] problems;
+
+  part2Problems = map (col:
+    let
+      new = map (i:
+        foldl' (acc: j:
+          let colElemnt = elemAt (stringToCharacters (elemAt col j)) i;
+          in if acc == "" then colElemnt else acc + colElemnt) ""
+        (range 0 ((length col) - 1))) (range 0 ((stringLength (head col)) - 1));
+    in new) paddedProblems;
+
+  part1 = compute paddedProblems;
+  part2 = compute part2Problems;
+
+in { inherit part1 part2; }
